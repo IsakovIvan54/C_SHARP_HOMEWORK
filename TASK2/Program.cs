3a7053468590c64fa1495fc6b159cae4bee1ea66
Ivan Isakov <wilcs702@gmail.com>
2023-04-24 22:30:09 +0300

    add first homework

---
+++ TASK2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int InFirstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int InSecondNum = Convert.ToInt32(Console.ReadLine());
+
+
+if (InFirstNum > InSecondNum){
+    Console.Write($"Число {InFirstNum} больше чем {InSecondNum}");
+}
+
+else if (InFirstNum < InSecondNum){
+    Console.Write($"Число {InSecondNum} больше чем {InFirstNum}");
+}
+else {
+    Console.Write("Введенные числа равны друг другу");
+}
---
+++ TASK4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+int InFirstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int InSecondNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число число: ");
+int InThirdNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (InFirstNum > InSecondNum && InFirstNum > InThirdNum){
+    Console.Write($"Число {InFirstNum} масимальное");
+}
+
+else if (InSecondNum > InFirstNum && InSecondNum > InThirdNum){
+    Console.Write($"Число {InSecondNum} масимальное");
+}
+
+else if (InThirdNum > InFirstNum && InThirdNum > InSecondNum){
+    Console.Write($"Число {InThirdNum} масимальное");
+}
+
+else {
+    Console.Write("Введенные числа равны друг другу");
+}
