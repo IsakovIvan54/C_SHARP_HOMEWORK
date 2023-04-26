f6988c99c936fb8a3f390e1ac2dfc9b783305702
IsakovIvan54 <131706902+IsakovIvan54@users.noreply.github.com>
2023-04-26 08:22:40 +0300

    add new function on TASK19

--- TASK19/Program.cs
+++ TASK19/Program.cs
@@ -13,13 +13,28 @@ Console.Write("Введите пятизначное число: ");
 
 int InNum = Convert.ToInt32(Console.ReadLine());
 
+int N = LenthNum(InNum);
+int[] Numbers = new int[N];
+
+
 int firstNum = InNum % 10;
 int secondNum = (InNum % 100 - InNum % 10) / 10;
 int fourNum = (InNum % 10000 - InNum % 1000) / 1000;
 int fifthNum = (InNum % 100000 - InNum % 10000) / 10000;
 
+int LenthNum(int ph){
+    int count = 0;
+        while(ph > 0) {
+        
+        
+        ph = (ph - ph % 10) / 10;
+        count++;
+}
+    return count;
+}
 
-
+Console.Write(LenthNum(InNum));
+Console.Write(Numbers[1]);
 
 if(InNum > 9999 && InNum < 100000){
      if (firstNum == fifthNum && secondNum == fourNum) {
