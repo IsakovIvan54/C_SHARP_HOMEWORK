8fc438d5165efcb3f98ec5506cec3631a6fe33b8
IvanIsakov <wildcs702@gmail.com>
2023-06-17 13:15:27 +0300

    add HomeWork 8

--- TASK41/Program.cs
+++ TASK41/Program.cs
@@ -8,14 +8,12 @@ Console.Write("Введите колчество чисел: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 int[] arr = new int[N];
-
-//int count_req = 0; // cчетчик введенных чисел
 int count_unsig = 0; // cчетчик чисел больше нуля
 
 for(int i = 0; i < N; i++){
     Console.Write("Введите число: ");
     arr[i] = Convert.ToInt32(Console.ReadLine());
-  //  count_req++;
+
     if(arr[i] > 0){
         count_unsig++;
     }
